cefce55ca6a1498af53d67b5cacd9ea5acf96fbc
Emil <emil.winberg1@gmail.com>
2024-11-15 13:49:53 +0100

    change line 42 in auth.ts

--- BackendAWS/src/routes/auth.ts
+++ BackendAWS/src/routes/auth.ts
@@ -67,7 +67,7 @@ app.get("/logout", (req: Request, res: Response) => {
   req.logout((err) => {
     if (err) {
       console.error("Error during logout:", err);
-      return res.status(500).json({ message: "Error logging out" });
+      return res.status(500).json({ message: "Error logging out hahahha" });
     }
     res.redirect("http://localhost:5173/");
   });
