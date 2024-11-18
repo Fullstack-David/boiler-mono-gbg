4de7398fabac865045515844c971e92a6ab12fbf
Emil <emil.winberg1@gmail.com>
2024-11-18 16:44:19 +0100

    row 42

--- FrontendAWS/eslint.config.js
+++ FrontendAWS/eslint.config.js
@@ -7,6 +7,7 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.node }},
+  {extends: ["eslint:recommended", "plugin:react/recommended"]},
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
